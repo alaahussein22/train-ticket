e636d4d337e039cb0270b5418dd77a015119d464
alaahussein22 <56509626+alaahussein22@users.noreply.github.com>
2022-12-16 23:27:25 +0200

    first

---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-app-bar
+      color="#00a88f accent-4"
+      dense
+      dark
+      style="padding: 2rem; background: #00a88f; border-radius: 2px"
+    >
+      <v-toolbar-title style="height: 75px">
+        <v-icon>mdi-train</v-icon>
+
+        trainline</v-toolbar-title
+      >
+
+      <v-spacer></v-spacer>
+
+      <v-row style="height: 88px">
+        <v-col> Basket </v-col>
+
+        <v-col> My Bookings </v-col>
+
+        <v-btn>Sign in</v-btn>
+      </v-row>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.v-toolbar__content {
+  height: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Navbar />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '../components/navbar.vue'
+  export default {
+    name: 'HomeC',
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+    components: {Navbar},
   }
-}
 </script>
